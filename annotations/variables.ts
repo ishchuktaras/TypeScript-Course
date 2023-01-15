31ea42010a0ff8ef35a997cbc5f950cac621c6ed
Taras Ishchuk <ishchuktaras@gmail.com>
2023-01-15 13:56:54 +0100

    Array, Classes, Object literal Annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -5,4 +5,23 @@ let hasDog: boolean = true;
 let nothing: null = null;
 let nothing1: undefined = undefined;
 
+// Built-in odjects
 let now: Date = new Date();
+
+//array
+let colors: string[] = ['red', 'green', 'blue'];
+let number: number[] = [1, 2, 3, 5];
+let resaults: boolean[] = [true, false];
+
+//classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+//object literal
+let point {x: number; y: number} = {
+  x: 10,
+  y: 20
+}
