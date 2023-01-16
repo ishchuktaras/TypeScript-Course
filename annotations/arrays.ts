762a82e65d74f3f7460827ebeb2467ff427ff361
Taras Ishchuk <ishchuktaras@gmail.com>
2023-01-16 19:03:26 +0100

    6. Typed Arrays

---
+++ annotations/arrays.ts
@@ -0,0 +1,15 @@
+const colors1 = ['red', 'green', 'yellow'];
+
+const dates = [new Date(), new Date()];
+
+const FruitsByColor = [
+  ['tomato'],
+  ['apple'],
+  ['lemon']
+];
+
+// Flexible types
+
+const importantDates = [new Date(), '2023-01-16'];
+importantDates.push ('2013-01-16');
+importantDates.push (new Date());
