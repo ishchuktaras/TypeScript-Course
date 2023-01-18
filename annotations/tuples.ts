89e2f4ce875f403feccab3a1186e681d613b96e9
Taras Ishchuk <ishchuktaras@gmail.com>
2023-01-18 20:11:42 +0100

    Tuples

---
+++ annotations/tuples.ts
@@ -0,0 +1,19 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40
+};
+
+// Type alias
+type Drink = [String, Boolean, Number]
+
+const cola: Drink = ['brown', true, 40];
+const coffee: Drink = ['black', false, 0];
+
+const carSpecs = [400, 3350];
+const carSpecsObj = {
+  horsepower: 400,
+  wheight: 3350
+}
+
+
